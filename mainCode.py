6c534e275677794e3fe5e799be9e6af087797bd2
Richard Couperthwaite <40215707+RichardCouperthwaite@users.noreply.github.com>
2019-03-21 22:42:46 -0500

    Image input
    Module to load img files and preprocess to the format required by the networks. Need to still add selection of training and test data, to reduce the size of the numpy arrays that are generated, it's taking too long to import the data

--- mainCode.py
+++ mainCode.py
@@ -13,7 +13,72 @@ import Test3
 import Test4
 import Test5
 import Test6
+from keras.backend import set_image_dim_ordering
+from keras.preprocessing.image import load_img, img_to_array
+from keras.applications.vgg19 import preprocess_input
+import numpy as np
+import json
+
+def load_micrograph(img_path):
+    set_image_dim_ordering('tf')
+    img = load_img(img_path)
+    x = img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+def get_parameter_levels(cropsData):
+    micros = []
+    cool = []
+    temp = []
+    time = []
+    
+    for label in cropsData:
+        if label != '_default':
+            if cropsData[label]['Primary_Microconstituent'] not in micros:
+                micros.append(cropsData[label]['Primary_Microconstituent'])
+            if cropsData[label]['Cool Method'] not in cool:
+                cool.append(cropsData[label]['Cool Method'])
+            if cropsData[label]['Anneal Time'] not in time:
+                time.append(cropsData[label]['Anneal Time'])
+            if cropsData[label]['Anneal Temperature'] not in temp:
+                temp.append(cropsData[label]['Anneal Temperature'])
+    return micros, cool, temp, time
+    
+    
 
 if __name__ == "__main__":
-    #run some code
-    pass
+    # obtain the dictionary of filenames for the cropped images in the UHCS dataset
+    with open('Micrograph_data.json', 'r') as f:
+        cropsData = json.load(f)
+        
+    # function can be run to confirm the levels hard coded below
+    #micros, cool, temp, time = get_parameter_levels(cropsData)
+    
+    # Hard coded levels for the parameters of interest
+    #micros = ['spheroidite', 'pearlite+spheroidite', 'network', 'spheroidite+widmanstatten', 'pearlite', 'pearlite+widmanstatten', 'martensite']
+    #cool = ['Q', 'N/A', 'AR', '650-1H', 'FC']
+    #temp = [800.0, 0, 970.0, 1100.0, 900.0, 1000.0, 700.0, 750.0]
+    #time = [90.0, 0, 180.0, 1440.0, 5100.0, 60.0, 5.0, 480.0, 2880.0]
+    
+    index = 0
+    
+    for label in cropsData:
+        if label != '_default':
+            if cropsData[label]['Cool Method'] != 'N/A':
+                new_img = load_micrograph(cropsData[label]['Path'])
+                if index == 0:
+                    inputs = new_img
+                    y_micro = [cropsData[label]['Primary_Microconstituent']]
+                    y_cool = [cropsData[label]['Cool Method']]
+                    y_time =[cropsData[label]['Anneal Time']]
+                    y_temp = [cropsData[label]['Anneal Temperature']]
+                    index += 1
+                else:
+                    inputs = np.r_[inputs, new_img]
+                    y_micro.append(cropsData[label]['Primary_Microconstituent'])
+                    y_cool.append(cropsData[label]['Cool Method'])
+                    y_time.append(cropsData[label]['Anneal Time'])
+                    y_temp.append(cropsData[label]['Anneal Temperature'])
+                
+    print(y_cool)
