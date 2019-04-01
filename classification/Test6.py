5b55c478101765d0fc07acaeec1a5faf3d0b34b0
Richard Couperthwaite <40215707+RichardCouperthwaite@users.noreply.github.com>
2019-04-01 17:02:22 -0500

    Post analysis mods
    Modification to some of the test6 code to get more information

--- classification/Test6.py
+++ classification/Test6.py
@@ -15,6 +15,8 @@ import h5py
 from sklearn.linear_model import Lasso
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
@@ -36,12 +38,28 @@ def correlation_matrix_plot(filename, data):
     plt.close(fig)
     
 
-def principle_component(filename, data):
+def principle_component(filename, data, test_data):
     pca = PCA(n_components=4)
     pca.fit(data)
-    with open("results/PCA_results.txt", 'a') as f:
+    with open("results/PCA_results_notupload.txt", 'a') as f:
         out = pca.explained_variance_ratio_
         f.write("{} & {} & {} & {} & {} \\\\ \n".format(filename, round(out[0],2), round(out[1],2), round(out[2],2), round(out[3],2)))
+    try:
+        new_data = pca.transform(data)
+        new_testdata = pca.transform(test_data)
+        pca_num = []
+        pca_sum = 0
+        for i in range(4):
+            pca_num.append(i)
+            pca_sum += out[i]
+            if pca_sum >= 0.98:
+                break
+    except:
+        new_data = []
+        new_testdata = []
+    return new_data[:,pca_num], new_testdata[:,pca_num]
+            
+        
 
 def get_data(name):
     f = h5py.File(name, 'r')
@@ -78,7 +96,7 @@ def test6_regression_test():
         print(y_train.shape)
         
         correlation_matrix_plot(name, x_train)
-        principle_component(name, x_train)
+        principle_component(name, x_train, x_test)
         
         lasso_reg = Lasso(alpha=0.1)
         lasso_reg.fit(x_train, y_train)
@@ -95,7 +113,58 @@ def test6_regression_test():
         
         with open('results/regressiontest.txt', 'a') as f:
             f.write("{}, {}, {}, {} \n".format(name, score2, score3, score4))
+            
+def testPCA():
+    filenames = ['test1_model1_temp.hdf5', 'test1_model2_temp.hdf5', 'test1_model3_temp.hdf5',
+                 'test1_model4_temp.hdf5', 'test1_model5_temp.hdf5', 'test1_model6_temp.hdf5',
+                 'test1_model7_temp.hdf5', 'test1_model8_temp.hdf5', 'test1_model9_temp.hdf5',
+                 'test1_model10_temp.hdf5', 'test1_model11_temp.hdf5', 'test1_model12_temp.hdf5',
+                 'test2_model1_temp.hdf5', 'test2_model2_temp.hdf5', 'test2_model3_temp.hdf5',
+                 'test2_model4_temp.hdf5', 'test2_model5_temp.hdf5', 'test2_model6_temp.hdf5',
+                 'test3_model1_temp.hdf5', 'test3_model2_temp.hdf5', 'test4_model1_temp.hdf5',
+                 'test5_model1_temp.hdf5', 'test5_model2_temp.hdf5', 'test5_model3_temp.hdf5',
+                 'test5_model4_temp.hdf5', 'test5_model5_temp.hdf5', 'test5_model6_temp.hdf5',
+                 'test1_model1_time.hdf5', 'test1_model2_time.hdf5', 'test1_model3_time.hdf5',
+                 'test1_model4_time.hdf5', 'test1_model5_time.hdf5', 'test1_model6_time.hdf5',
+                 'test1_model7_time.hdf5', 'test1_model8_time.hdf5', 'test1_model9_time.hdf5',
+                 'test1_model10_time.hdf5', 'test1_model11_time.hdf5', 'test1_model12_time.hdf5',
+                 'test2_model1_time.hdf5', 'test2_model2_time.hdf5', 'test2_model3_time.hdf5',
+                 'test2_model4_time.hdf5', 'test2_model5_time.hdf5', 'test2_model6_time.hdf5',
+                 'test3_model1_time.hdf5', 'test3_model2_time.hdf5', 'test4_model1_time.hdf5',
+                 'test5_model1_time.hdf5', 'test5_model2_time.hdf5', 'test5_model3_time.hdf5',
+                 'test5_model4_time.hdf5', 'test5_model5_time.hdf5', 'test5_model6_time.hdf5']
+    for name in filenames:
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print(name)
+        x_train, x_test, y_train, y_test = get_data("results/"+name)
+        
+        correlation_matrix_plot(name, x_train)
+        new_x_train, new_x_test = principle_component(name, x_train, x_test)
+        
+        gpr = GaussianProcessRegressor(kernel=RBF(), random_state=0).fit(new_x_train, y_train)
+        score1 = gpr.score(new_x_test, y_test)
+        print('##### GP done #####')
+        
+        lasso_reg = Lasso(alpha=0.1)
+        lasso_reg.fit(new_x_train, y_train)
+        score2 = lasso_reg.score(new_x_test, y_test)
+        print('##### Lasso done #####')
+        
+        try:
+            svr_reg = SVR(gamma='scale', C=1.0, epsilon=0.2)
+            svr_reg.fit(new_x_train, y_train)
+            score3 = svr_reg.score(new_x_test, y_test)
+        except:
+            score3 = -20 
+        print('##### SVR done #####')
+        
+        rf_reg = RandomForestRegressor(max_depth=2, random_state=0, n_estimators=100)
+        rf_reg.fit(new_x_train, y_train)
+        score4 = rf_reg.score(new_x_test, y_test)
+        print('##### RF done #####')
+        print("{}, {}, {}, {}, {} \n".format(name, score1, score2, score3, score4))
+        with open('results/regressiontest_afterPCA.txt', 'a') as f:
+            f.write("{}, {}, {}, {}, {} \n".format(name, score1, score2, score3, score4))
 
 if __name__ == "__main__":
-    #run some code
-    pass
+    testPCA()
